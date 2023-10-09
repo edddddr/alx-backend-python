af3d553926d38199c5a752c4366f213e5543913c
edddddr <endt6342@gmail.com>
2023-10-09 10:30:53 +0000

    async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Basic syntax await async """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+        Args:
+            max_delay: max sleep
+        Return:
+            float time random
+    """
+    rand: float = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+
+    return rand
